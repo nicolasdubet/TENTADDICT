1672d09c845e672b8599359867592b1a1eba861a
christophe mendy <christophe.mendy@kedgebs.com>
2021-05-17 16:37:45 +0200

    back tent controller

--- app/controllers/tents_controller.rb
+++ app/controllers/tents_controller.rb
@@ -1,2 +1,25 @@
 class TentsController < ApplicationController
+    def index
+      @tents = Tent.all
+    end
+    def show
+    @tent = Tent.find(params[:id])
+    end
+    def new
+    @tent = Tent.new   
+    end
+    def create
+        @tent = Tent.new(tent_params)
+        if @tent.save
+            redirect_to tent_path(@tent)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def tent_params
+      params.require(:tent).permit(:name, :number_of_couch, :season, :weight)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'tents#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tents, except: [:edit, :update, :destroy]
 end
