e1744f1a0f3e0087c0d309dc01c3d659d3639b5c
nicolasdubet <nicolas.dubet@orange.fr>
2021-05-19 15:24:54 +0200

    booking

--- app/models/tent.rb
+++ app/models/tent.rb
@@ -3,4 +3,5 @@ class Tent < ApplicationRecord
   validates :number_of_couch, presence: true, inclusion: 1..10
   validates :season, presence: true, inclusion: { in: %w(spring/summer fall/winter 4seasons), message: "%{value} is not a season" }
   validates :weight, presence: true, inclusion: 1..7
+  has_one_attached :photo
 end
