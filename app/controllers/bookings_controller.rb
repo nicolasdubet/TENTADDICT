3b27654c17db3155916702a087e0ca2273baac64
nicolasdubet <nicolas.dubet@orange.fr>
2021-05-20 12:21:53 +0200

    booking csscontainer

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: [:show]
+  before_action :set_booking, only: [:show, :destroy]
   before_action :set_tent, only: [:new, :create]
 
     def index
@@ -7,7 +7,7 @@ class BookingsController < ApplicationController
     end
 
     def show
-      @tent = booking.tent  
+      @tent = booking.tent
     end
 
     def new
@@ -23,6 +23,11 @@ class BookingsController < ApplicationController
       else
         render :new
       end
+    end
+
+    def destroy
+      @booking.destroy
+      redirect_to bookings_path
     end
 
     private
